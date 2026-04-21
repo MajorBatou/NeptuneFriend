9eb18033aeddae5326e98e15aebf961c5a8a5322
Nkfto Batou <themajorbatou3@gmail.com>
2026-04-21 08:40:17 -0400

    feat(terraform): add EBS CSI driver addon to EKS cluster

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -101,3 +101,10 @@ resource "aws_eks_node_group" "main" {
     Name = "${var.project}-${var.environment}-node-group"
   }
 }
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "aws-ebs-csi-driver"
+
+  depends_on = [aws_eks_node_group.main]
+}
